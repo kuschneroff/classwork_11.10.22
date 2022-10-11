eccb9f85310637b6660205cb58ca26cddace1403
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-11 11:13:10 +0300

    Task_#2

---
+++ Task_#2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5 */
+
+Console.WriteLine("Введите число");
+
+int number = int.Parse(Console.ReadLine() ??"");
+int count = 0;
+
+while(number > 0)
+
+{
+    count = count + 1;
+    number = number /10;
+}
+Console.WriteLine($"Кол-во цифр в числе {count}");
