0650a3fff2a53840dda1869b7e93edcda8fec98e
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-11 21:13:13 +0300

    Task_#3

--- Task_#3/Program.cs
+++ Task_#3/Program.cs
@@ -1,4 +1,9 @@
-﻿void GetProd()
+﻿/*
+Напиште программу, которая принимает на вход число N и выдает произведение числе от 1 до N.
+*/
+
+
+void GetProd()
 {
     Console.WriteLine("Введите число N: ");
     int num = int.Parse(Console.ReadLine() ??"");
