615527be9e70c75e071ba3c271824c3ad0657f23
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-11 11:13:45 +0300

    Task_#4

---
+++ Task_#4/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]*/
+
+int[] CreateArray(int n)
+{
+int[] array=new int[n];
+Random rnd = new Random();
+for(int i=0;i<array.Length;i++)
+{
+    array[i]= rnd.Next(0,2);
+}
+return array;
+}
+
+int[]array= CreateArray(10);
+for (int i = 0; i < array.Length; i++)
+{
+   Console.Write($"{array[i]} ") ;
+}
