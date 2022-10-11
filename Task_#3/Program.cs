383b05755e668757ad1a7f1f562f2c99864e425b
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-11 11:13:19 +0300

    Task_#3

---
+++ Task_#3/Program.cs
@@ -0,0 +1,13 @@
+﻿void GetProd()
+{
+    Console.WriteLine("Введите число N: ");
+    int num = int.Parse(Console.ReadLine() ??"");
+    int prod = 1;
+    for (int  i  = 1; i <= num; i ++ )
+    {
+        prod = prod * i;
+    }
+    
+    Console.WriteLine($"{num} -> {prod}");
+}
+GetProd();
