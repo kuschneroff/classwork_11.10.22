fa4d357b42b5d0824aff1229887c91ccea45c284
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-11 11:13:01 +0300

    Task_#1

---
+++ Task_#1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+
+//метод который получает число с консоли
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine() ??"");
+    return result;
+}
+
+int GetSumm(int a)
+{
+    int summ = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        summ += i;
+    }
+    return summ;
+}
+
+int a = GetNumber("Введите число А:");
+int summ = GetSumm(a);
+
+Console.WriteLine($"Сумма чисел от 1 до {a} = {summ}");
